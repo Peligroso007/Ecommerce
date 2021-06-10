e765baab126171f38ec32f7066db3fae8608ddae
Peligroso007 <milanbashyal123@gmail.com>
2021-06-10 12:28:02 +0545

    signup done.

--- MBEcommerce/settings.py
+++ MBEcommerce/settings.py
@@ -94,6 +94,16 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 
 ]
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'milanbashyal2055@gmail.com'
+EMAIL_HOST_PASSWORD = '9848049694@gmail'
+
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 
